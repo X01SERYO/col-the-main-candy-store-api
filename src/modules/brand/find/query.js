97b583507a7c48e15fcd7a00ed4cfc648e0359c7
felipe131305 <felipe08119617@gmail.com>
2022-11-27 11:48:17 -0500

    style: comment correction

--- src/modules/brand/find/query.js
+++ src/modules/brand/find/query.js
@@ -1,11 +1,16 @@
+/**
+ * Represents a brand
+ */
 class Query {
   /**
    * Creates new find brand command
-   * @param {*} id
-   * @param {*} name
-   * @param {*} code
-   * @param {*} page
-   * @param {*} size
+   * /**
+   * Creates new create brand command
+   * @param {String} id The brand id
+   * @param {String} name The brand name
+   * @param {String} code The brand code
+   * @param {Number} page The brand page
+   * @param {Number} sizeThe brand size
    */
   constructor({ id, name, code, page, size }) {
     this.name = name;
