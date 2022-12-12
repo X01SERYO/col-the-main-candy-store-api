7ac2dbff30133136aaaf7798c6b112eafa625faf
X01SERYO <sergios@keoworld.com>
2022-12-12 10:25:09 -0500

    style: applies npm run format

--- src/infra/db/mongoose.js
+++ src/infra/db/mongoose.js
@@ -7,7 +7,7 @@ const connection = async () => {
   const username = 'mcs';
   const password = 'MSLKRA2Rf0fRDgvi';
   const database = 'develop';
-  const uri = `mongodb+srv://${username}:${password}@develop.yyja1u8.mongodb.net/${database}?retryWrites=true&w=majority`
+  const uri = `mongodb+srv://${username}:${password}@develop.yyja1u8.mongodb.net/${database}?retryWrites=true&w=majority`;
   const connectionParams = { useNewUrlParser: true, useUnifiedTopology: true };
   try {
     await mongoose.connect(uri, connectionParams);
