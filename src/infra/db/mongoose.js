a37cd356dd945864f61384af932c3006cd4ba337
X01SERYO <sergio.s16a@hotmail.com>
2023-09-03 08:48:07 -0500

    fix: mongo connection

--- src/infra/db/mongoose.js
+++ src/infra/db/mongoose.js
@@ -5,10 +5,10 @@ import { config, logger } from './../../infra';
  * Make connection with mongodb
  */
 const connection = async () => {
-  const username = config.db.mongo.user;
-  const password = config.db.mongo.password;
+  const username = encodeURIComponent(config.db.mongo.user);
+  const password = encodeURIComponent(config.db.mongo.password);
   const database = config.db.mongo.database;
-  const uri = `mongodb+srv://${username}:${password}@develop.yyja1u8.mongodb.net/${database}?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://${username}:${password}@${database}.yyja1u8.mongodb.net/retryWrites=true&w=majority`;
   const connectionParams = { useNewUrlParser: true, useUnifiedTopology: true };
   try {
     await mongoose.connect(uri, connectionParams);
